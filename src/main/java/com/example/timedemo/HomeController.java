1ddb8107c9bbc83797b1f4e1732aec87e73ce9df
GBTC440015UR <GBTC440015UR@MC.MONTGOMERYCOLLEGE.EDU>
2020-02-18 16:05:53 -0500

    first commit

---
+++ src/main/java/com/example/timedemo/HomeController.java
@@ -0,0 +1,28 @@
+package com.example.timedemo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.thymeleaf.extras.java8time.dialect.Java8TimeDialect;
+import org.thymeleaf.spring5.ISpringTemplateEngine;
+import org.thymeleaf.spring5.SpringTemplateEngine;
+import org.thymeleaf.templateresolver.ITemplateResolver;
+
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Date;
+
+@Controller
+public class HomeController {
+    @RequestMapping("/")
+    public String index(Model model){
+        model.addAttribute("standardDate", new Date());
+        model.addAttribute("localDateTime", LocalDateTime.now());
+        model.addAttribute("localDate", LocalDate.now());
+        model.addAttribute("timestamp", Instant.now());
+
+        return "index";
+    }
+
+}
---
+++ src/test/java/com/example/timedemo/TimedemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.timedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimedemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
